0116e5e72e63ba4dd8d63e5ce45931fdabad29eb
shekhar <shekharjha7417870578@gmail.com>
2022-08-06 14:15:01 +0530

    added uc2

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SnakeandLadder {
     static int POSITION=0;
     static int TOTAL_DIE_ROLL=0;
@@ -7,4 +9,12 @@ public class SnakeandLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And ladder");
         System.out.println("Current Position : " + POSITION);
-}
+
+        private static int dieRoll () {
+            Random random = new Random();
+            int dieNumber = random.nextInt(6) + 1;
+            TOTAL_DIE_ROLL++;
+            return dieNumber;
+        }
+    }}
+
