d7e04f63a7bec7210a4064f824b55439f6b8b286
shekhar <shekharjha7417870578@gmail.com>
2022-08-06 14:18:18 +0530

    added uc3

--- src/SnakeandLadder.java
+++ src/SnakeandLadder.java
@@ -10,6 +10,25 @@ public class SnakeandLadder {
         System.out.println("Welcome to Snake And ladder");
         System.out.println("Current Position : " + POSITION);
 
+        private static void action(int dieNumber) {
+            int option = (int) (Math.random() * 10) % 3;
+            switch (option){
+                case IS_LADDER:
+                    System.out.println("Yay... you got a Ladder");
+                    if (POSITION+dieNumber<=100)
+                        POSITION+=dieNumber;
+                    break;
+                case IS_SNAKE:
+                    System.out.println("Oops it's a Snake");
+                    POSITION-=dieNumber;
+                    if (POSITION<0)
+                        POSITION =0;
+                    break;
+                default:
+                    System.out.println("No Play");
+            }
+        }
+
         private static int dieRoll () {
             Random random = new Random();
             int dieNumber = random.nextInt(6) + 1;
